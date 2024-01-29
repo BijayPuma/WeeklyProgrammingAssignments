b925b79b1115fbb5c0b71f9da7eca05e724b01d3
BijayRai <raibjraibj@yahoo.com>
2024-01-29 00:34:11 -0600

    Added modifers on the button label and removed multiple repetative VStack and combined into single VStack for all sliders

--- ColorPicker/ContentView.swift
+++ ColorPicker/ContentView.swift
@@ -8,16 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var redValue = 127.0
+    
+    @State private var redValue = 47.0
     @State private var greenValue = 127.0
-    @State private var blueValue = 127.0
-    @State private var setColor = Color(red: 127.5 / 255, green: 127.5 / 255, blue: 127.5 / 255)
+    @State private var blueValue = 247.0
+    @State private var setColor = Color(red: 47.5 / 255, green: 127.5 / 255, blue: 233.5 / 255)
     
     
     var body: some View {
+        
         VStack {
          Text("Color Picker")
                 .font(.title)
+            
          RoundedRectangle(cornerRadius: 0)
                 .frame(width: 300, height: 300)
                 .foregroundColor(setColor)
@@ -28,26 +31,28 @@ struct ContentView: View {
                     Slider(value: $redValue, in: 1.0...255.0)
                     Text("255")
                 }
-            }
-            VStack {
                 Text("Green \(Int(greenValue))")
                 HStack{
                     Slider(value: $greenValue, in: 1.0...255.0)
                     Text("255")
                 }
-            }
-            VStack {
+                
                 Text("Blue \(Int(blueValue))")
                 HStack{
                     Slider(value: $blueValue, in: 1.0...255.0)
                     Text("255")
                 }
             }
+
     
             Button {
                 self.setColor = Color(red: redValue / 255, green: greenValue / 255, blue: blueValue / 255)
             }label: {
                 Text("Set Color")
+                    .frame(width: 100, height: 40)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
             }
         }
         .padding()
