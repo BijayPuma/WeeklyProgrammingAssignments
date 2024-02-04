bd1df2a9d9bbcf49ec4868aacb3b601b8487e990
BijayRai <raibjraibj@yahoo.com>
2024-02-04 07:10:18 -0600

    part3 done

--- week2/ColorPicker/Views/ContentView.swift
+++ week2/ColorPicker/Views/ContentView.swift
@@ -7,19 +7,39 @@ struct ContentView: View {
   @State private var greenColor: Double = Constants.initialGreenColorValue
   @State private var blueColor: Double = Constants.initialBlueColorValue
   @State private var foregroundColor = Color(red: Constants.initialRedColorValue, green: Constants.initialGreenColorValue, blue: Constants.initialBlueColorValue)
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+  @Environment(\.verticalSizeClass) var verticalSizeClass
 
   var body: some View {
 
     ZStack {
       Color("BackgroundColor").ignoresSafeArea()
-      VStack {
-        HeaderTextView(text: "Color Picker")
-        ColorDisplayRectView(foregroundColor: $foregroundColor)
-        SliderView(redColor: $redColor, greenColor: $greenColor, blueColor: $blueColor)
-    
-        ButtonSetView(redColor: $redColor, greenColor: $greenColor, blueColor: $blueColor, foregroundColor: $foregroundColor)
+      
+      if horizontalSizeClass == .compact && verticalSizeClass == .regular {
+        VStack {
+          HeaderTextView(text: "Color Picker")
+          ColorDisplayRectView(foregroundColor: $foregroundColor)
+          SliderView(redColor: $redColor, greenColor: $greenColor, blueColor: $blueColor)
+      
+          ButtonSetView(redColor: $redColor, greenColor: $greenColor, blueColor: $blueColor, foregroundColor: $foregroundColor)
+        }
+       .padding(20)
+      } else {
+        HStack {
+          VStack {
+            HeaderTextView(text: "Color Picker")
+            ColorDisplayRectView(foregroundColor: $foregroundColor)
+          }
+          .padding()
+          VStack {
+            SliderView(redColor: $redColor, greenColor: $greenColor, blueColor: $blueColor)
+        
+            ButtonSetView(redColor: $redColor, greenColor: $greenColor, blueColor: $blueColor, foregroundColor: $foregroundColor)
+          }
+          .padding()
+        }
       }
-     .padding(20)
+
     }
 
   }
