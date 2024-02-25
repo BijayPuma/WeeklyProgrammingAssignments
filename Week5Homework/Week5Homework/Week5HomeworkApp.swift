37ef215293e0bdba4a20f6a27d9bd95209263b34
BijayRai <raibjraibj@yahoo.com>
2024-02-25 11:44:49 -0600

    Initial commit

---
+++ Week5Homework/Week5Homework/Week5HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week5HomeworkApp.swift
+//  Week5Homework
+//
+//  Created by Bijay Rai on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Week5HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
