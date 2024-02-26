c95b7bb1bcb7045e718cbfac8d46a2f258f56def
BijayRai <raibjraibj@yahoo.com>
2024-02-25 22:55:38 -0600

    Initial commit

---
+++ WeekFive/WeekFive/WeekFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekFiveApp.swift
+//  WeekFive
+//
+//  Created by Bijay Rai on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeekFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
